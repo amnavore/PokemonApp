2b009a198ce13ecb1fd892a4b02afef9cc29139b
Amna Bhatti <abhatti1@hawk.iit.edu>
2019-02-17 23:36:18 -0600

    Add validations for pokemon and pokestop model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,4 @@
 class Pokemon < ApplicationRecord
   has_and_belongs_to_many:Pokestop
+  validates :name, :location, presence:true
 end
--- app/models/pokestop.rb
+++ app/models/pokestop.rb
@@ -1,3 +1,4 @@
 class Pokestop < ApplicationRecord
   has_and_belongs_to_many:Pokemon
+  validates :name, :location, presence:true
 end
